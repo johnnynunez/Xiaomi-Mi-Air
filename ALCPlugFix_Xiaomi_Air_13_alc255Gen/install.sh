7ee09aa68cdc4cd221721933a593ba874030a814
johnnync13 <johnnynuca14@gmail.com>
2020-01-14 10:02:25 +0100

    New Script audio, updated CodecCommander,USBInjectAll and ALCPLUGINFIX @Sniki

--- ALCPlugFix_Xiaomi_Air_13_alc255Gen/install.sh
+++ ALCPlugFix_Xiaomi_Air_13_alc255Gen/install.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 
 DAEMON_PATH=/Library/LaunchDaemons/
-BIN_PATH=/usr/bin/
+BIN_PATH=/usr/local/bin/
 DAEMON_FILE=good.win.ALCPlugFix.plist
 VERB_FILE=hda-verb
 FIX_FILE=ALCPlugFix
 
-echo "Installing ALCPlugFix.  Root user is required."
+echo "Installing ALCPlugFix v1.6"
 
-# check if the root filesystem is writeable (starting with macOS 10.15 Catalina, the root filesystem is read-only by default)
-if sudo test ! -w "/"; then
-    echo "Root filesystem is not writeable.  Remounting as read-write and restarting Finder."
-    sudo mount -uw /
-    sudo killall Finder
+# check if the directory "usr/local/bin" exist, if not then create the directory
+
+if [ ! -d "$BIN_PATH" ] ; then 
+mkdir "$BIN_PATH" ; 
 fi
 
 # stop the daemon if it's already running
@@ -22,6 +21,7 @@ if sudo launchctl list | grep --quiet ALCPlugFix; then
 fi
 
 # copy over the files to their respective locations (overwrite automatically if files exist)
+
 sudo cp -f ALCPlugFix $BIN_PATH
 sudo cp -f hda-verb $BIN_PATH
 sudo cp -f good.win.ALCPlugFix.plist $DAEMON_PATH
--- ALCPlugFix_Xiaomi_Air_13_alc255Gen/uninstall.sh
+++ ALCPlugFix_Xiaomi_Air_13_alc255Gen/uninstall.sh
@@ -14,6 +14,8 @@ sudo rm /usr/bin/hda-verb
 sudo launchctl unload -w /Library/LaunchDaemons/good.win.ALCPlugFix.plist
 sudo launchctl remove good.win.ALCPlugFix
 sudo rm /Library/LaunchDaemons/good.win.ALCPlugFix.plist
+sudo rm /usr/local/bin/ALCPlugFix
+sudo rm /usr/local/bin/hda-verb
 
 echo "Done!"
 exit 0
